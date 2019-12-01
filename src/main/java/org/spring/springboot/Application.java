b3ee33a33e3cde5469ccf8f9ce95d25e11816ed1
xinxi <xinxi@luojilab.com>
2019-12-01 19:53:37 +0800

    first commit

---
+++ src/main/java/org/spring/springboot/Application.java
@@ -0,0 +1,38 @@
+package org.spring.springboot;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.spring.springboot.dao.CityDao;
+import org.spring.springboot.domain.City;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+import springfox.documentation.service.Contact;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+
+/**
+ * Spring Boot 应用启动类
+ *
+ */
+// Spring Boot 应用的标识
+@SpringBootApplication
+// mapper 接口类扫描包配置
+@MapperScan("org.spring.springboot.dao")
+public class Application {
+
+    public static void main(String[] args) {
+        // 程序启动入口
+        // 启动嵌入式的 Tomcat 并初始化 Spring 环境及其各 Spring 组件
+        SpringApplication.run(Application.class,args);
+    }
+}
